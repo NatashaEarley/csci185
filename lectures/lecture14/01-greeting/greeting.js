007d16e007b136701586f4ae960c467fe490e98b
Natasha <nearley@unca.edu>
2024-03-18 12:18:00 -0400

    Lecture14

--- lectures/lecture14/01-greeting/greeting.js
+++ lectures/lecture14/01-greeting/greeting.js
@@ -1,7 +1,9 @@
 function sayHello() {
-    // your code here...
+    const myElement = document.querySelector("#message");
+    myElement.innerHTML = "Hello!"
 }
 
 function sayGoodbye() {
-    // your code here...
+    const myElement = document.querySelector("#message");
+    myElement.innerHTML = "See ya!"
 }
