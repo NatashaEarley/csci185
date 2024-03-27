916d97aef16151e85dea7ee7a19f9c11f90c91cf
Natasha <nearley@unca.edu>
2024-03-27 12:56:55 -0400

    tutorial 17 and final project1b

--- lectures/lecture16/03-drawings/sketch.js
+++ lectures/lecture16/03-drawings/sketch.js
@@ -20,18 +20,35 @@ function setup() {
     rect(30, 20, 55, 55);
 
     // add your drawing here:
-    // https://p5js.org/reference/#/p5/point
-    // point(110, 20);
+    https://p5js.org/reference/#/p5/point
+    strokeWeight(5)
+    point(110, 20);
 
     // https://p5js.org/reference/#/p5/text
-    // text("Here is some text", 200, 40);
+    fill("black")
+    text("Here is some text", 200, 40);
 
-    // https://p5js.org/reference/#/p5/ellipse
-    // fill('red');
-    // ellipse(100, 200, 60, 100);
+    https://p5js.org/reference/#/p5/ellipse
+    fill('red');
+    ellipse(100, 200, 60, 100);
+
+    fill("blue");
+    rect(200,300,200,200);
+    triangle(300,200,200,300,400,300)
+    rect(250,400,100,100)
+    rect(225,325,50,50)
+    rect(325,325,50,50)
+    line(350,325,350,375)
+    line(250,325,250,375)
+    line(225,350,275,350)
+    line(325,350,375,350)
+    strokeWeight(12)
+    point(320,450)
+
+
 
     // Other shapes...
-    // Polygon: https://p5js.org/reference/#/p5/beginShape
+    Polygon: https://p5js.org/reference/#/p5/beginShape
     // Line: https://p5js.org/reference/#/p5/line
     
     // Curve: https://p5js.org/reference/#/p5/curve
---
+++ lectures/lecture17/01-light-bulb/script.js
@@ -0,0 +1,13 @@
+let isOn = false;
+
+function turnOnOff() {
+    if (isOn === false) {
+        document.querySelector("#my_image").src = "bulb_on.gif";
+        isOn = true;
+        console.log(isOn);
+    } else {
+        document.querySelector("#my_image").src = "bulb_off.gif";
+        isOn = false;
+        console.log(isOn)
+    }
+}
