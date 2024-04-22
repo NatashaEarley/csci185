8473487011926919222ca16fdb7d8f8ddf24ae28
Natasha <nearley@unca.edu>
2024-04-22 16:36:07 -0400

    lecture20 and tut 10

---
+++ tutorials/tutorial11/exercise01/main.js
@@ -0,0 +1,18 @@
+
+function makeRed() {
+    document.querySelector("#box").style.backgroundColor = "red";
+
+}
+
+function makeBlue() {
+    document.querySelector("#box").style.backgroundColor = "blue";
+}
+
+function makePink() {
+    document.querySelector("#box").style.backgroundColor = "pink";
+}
+
+function makeOrange() {
+    document.querySelector("#box").style.backgroundColor = "orange";
+}
+
---
+++ tutorials/tutorial11/exercise02/main.js
@@ -0,0 +1,20 @@
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight;
+
+colors = ['blue', 'red', 'teal', 'mint']
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+    drawShape(100, 100, 150, '#db5461', '#102e4a');
+    drawShape(200, 200, 75, '#102e4a', '#8093f1');
+    drawShape(100, 325, 100, '#8093f1', '#7fb285');
+    drawShape(250, 375, 125, '#7fb285', '#0bc9cd');
+    drawShape(450, 200, 250, '#0bc9cd', '#db5461');
+    drawGrid(canvasWidth, canvasHeight);
+}
+
+function drawShape(x, y, size, bigColor, smallColor) {
+    fill(bigColor);
+    circle(x, y, size);
+    fill(smallColor);
+    circle(x, y, size/2);
+}
