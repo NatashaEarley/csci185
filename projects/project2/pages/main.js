606a5227bd19613dc57d18aa4a5b937d985e3ed7
NatashaEarley <nearley@unca.edu>
2024-05-07 23:47:04 -0400

    fin

--- projects/project2/pages/main.js
+++ projects/project2/pages/main.js
@@ -1,31 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var swiper1 = new Swiper(".mySwiper", { 
+    var swiper = new Swiper(".mySwiper", {
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
         pagination: {
-            el: ".swiper-pagination", 
-        },
-        loop: true 
-    });
-
-    var swiper2 = new Swiper(".mySwiper2", { 
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-            el: ".swiper-pagination2", 
+            el: ".swiper-pagination",
             type: "progressbar",
         },
         loop: true 
-    })
-    var swiper3 = new Swiper(".swiper-slide3", {
-
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
     });
 });
